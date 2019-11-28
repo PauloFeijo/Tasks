9c2080c519def5e73f84a0a4721ab05fb45d6b11
PauloFeijo <paulorikrdo.89@gmail.com>
2019-11-27 23:12:46 -0300

    Validações autenticação

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -23,39 +23,58 @@ export default class Auth extends Component {
         confirmPassword: ''
     }
 
-    signinOrSignup = async () => {
+    signin = async () => {
+        try {
+            await axios.post(`${server}/signup`, {
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+                confirmPassword: this.state.confirmPassword
+            })
+
+            Alert.alert('Sucesso!', 'Usuário cadastrado com sucesso :)')
+            this.setState({ stageNew: false })
+        } catch (err) {
+            showError(err)
+        }
+    }
+
+    signup = async () => {
+        try {
+            const res = await axios.post(`${server}/signin`, {
+                email: this.state.email,
+                password: this.state.password,
+            })
+
+            axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
+
+            this.props.navigation.navigate('Home')
+
+        } catch (error) {
+            Alert.alert('Erro', 'Falha no Login!')
+        }
+    }
+
+    signinOrSignup = () => {
         if (this.state.stageNew) {
-            try {
-                await axios.post(`${server}/signup`, {
-                    name: this.state.name,
-                    email: this.state.email,
-                    password: this.state.password,
-                    confirmPassword: this.state.confirmPassword
-                })
-
-                Alert.alert('Sucesso!', 'Usuário cadastrado com sucesso :)')
-                this.setState({ stageNew: false })
-            } catch (err) {
-                showError(err)
-            }
+            this.signin()
         } else {
-            try {
-                const res = await axios.post(`${server}/signin`, {
-                    email: this.state.email,
-                    password: this.state.password,
-                })
-
-                axios.defaults.headers.common['Authorization'] = `bearer ${res.data.token}`
-
-                this.props.navigation.navigate('Home')
-
-            } catch (error) {
-                Alert.alert('Erro', 'Falha no Login!')
-            }
+            this.signup()
         }
     }
 
     render() {
+        const validations = []
+        validations.push(this.state.email && this.state.email.includes('@'))
+        validations.push(this.state.password && this.state.password.length >= 6)
+
+        if (this.state.stageNew) {
+            validations.push(this.state.name && this.state.name.trim())
+            validations.push(this.state.confirmPassword)
+            validations.push(this.state.password === this.state.confirmPassword)
+        }
+
+        const validForm = validations.reduce((all, v) => all && v)
         return (
             <ImageBackground source={backgroundImage} style={styles.background}>
 
@@ -96,8 +115,9 @@ export default class Auth extends Component {
                             value={this.state.confirmaPassword}
                             onChangeText={confirmPassword => this.setState({ confirmPassword })} />}
 
-                    <TouchableOpacity onPress={this.signinOrSignup}>
-                        <View style={styles.button}>
+                    <TouchableOpacity disabled={!validForm}
+                        onPress={this.signinOrSignup}>
+                        <View style={[styles.button, !validForm ? { backgroundColor: '#AAA' } : {}]}>
                             <Text style={styles.buttonText}>
                                 {this.state.stageNew ? 'Registrar' : 'Entrar'}
                             </Text>
