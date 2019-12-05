8a6f1c3a31fd73d4d52b450e0796c5ec04283865
PauloFeijo <paulorikrdo.89@gmail.com>
2019-12-04 23:52:41 -0300

    Menu filtrar tarefas

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -13,6 +13,9 @@ import { server, showerror, showError } from '../common'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 import todayImage from '../../assets/imgs/today.jpg'
+import tomorrowImage from '../../assets/imgs/tomorrow.jpg'
+import weekImage from '../../assets/imgs/week.jpg'
+import monthImage from '../../assets/imgs/month.jpg'
 import commonStyles from '../commonStyles'
 import Task from '../components/Task'
 import Icon from 'react-native-vector-icons/FontAwesome'
@@ -80,7 +83,9 @@ export default class Agenda extends Component {
 
     loadTasks = async () => {
         try {
-            const maxDate = moment().format('YYYY-MM-DD 23:59')
+            const maxDate = moment()
+                .add({ days: this.props.daysAhead })
+                .format('YYYY-MM-DD 23:59')
             const res = await axios.get(`${server}/tasks?date=${maxDate}`)
             this.setState({ tasks: res.data }, this.filterTasks)
         } catch (err) {
@@ -89,26 +94,56 @@ export default class Agenda extends Component {
     }
 
     render() {
+        let styleColor = null
+        let image = null
+
+        switch (this.props.daysAhead) {
+            case 0:
+                styleColor = commonStyles.colors.today
+                image = todayImage
+                break
+            case 1:
+                styleColor = commonStyles.colors.tomorrow
+                image = tomorrowImage
+                break
+            case 7:
+                styleColor = commonStyles.colors.today
+                image = weekImage
+                break
+            default:
+                styleColor = commonStyles.colors.today
+                image = monthImage
+                break
+        }
+
         return (
             <View style={styles.container}>
+
                 <AddTask isVisible={this.state.showAddTask}
                     onSave={this.addTask}
                     onCancel={() => this.setState({ showAddTask: false })} />
-                <ImageBackground source={todayImage}
-                    style={styles.background}>
+
+                <ImageBackground source={image} style={styles.background}>
                     <View style={styles.iconBar}>
+
+                        <TouchableOpacity onPress={() => this.props.navigation.openDrawer()}>
+                            <Icon name='bars' size={20} color={commonStyles.colors.secondary} />
+                        </TouchableOpacity>
+
                         <TouchableOpacity onPress={this.toggleFilter}>
                             <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
                                 size={20} color={commonStyles.colors.secondary} />
                         </TouchableOpacity>
                     </View>
+
                     <View style={styles.titleBar}>
-                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.title}>{this.props.title}</Text>
                         <Text style={styles.subtitle}>
                             {moment().locale('pt-br').format('ddd. D [de] MMMM')}
                         </Text>
                     </View>
                 </ImageBackground>
+
                 <View style={styles.tasksContainer}>
                     <FlatList data={this.state.visibleTasks}
                         keyExtractor={item => `${item.id}`}
@@ -116,7 +151,7 @@ export default class Agenda extends Component {
                             <Task {...item} onToggleTask={this.toggleTask}
                                 onDelete={this.deleteTask} />} />
                 </View>
-                <ActionButton buttonCollor={commonStyles.colors.today}
+                <ActionButton buttonColor={styleColor}
                     onPress={() => { this.setState({ showAddTask: true }) }} />
             </View>
         )
@@ -155,6 +190,6 @@ const styles = StyleSheet.create({
         marginTop: Platform.OS === 'ios' ? 30 : 10,
         marginHorizontal: 20,
         flexDirection: 'row',
-        justifyContent: 'flex-end'
+        justifyContent: 'space-between'
     }
 })
